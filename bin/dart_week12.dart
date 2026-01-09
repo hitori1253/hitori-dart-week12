82bd6c5c2897258187f569d53676ba02c7ff40c2
hitori1253 <jont2514@gmail.com>
2026-01-09 11:37:57 +0700

    getter1

--- bin/dart_week12.dart
+++ bin/dart_week12.dart
@@ -1,10 +1,12 @@
 //import 'package:dart_week12/cat.dart';
 //import 'package:dart_week12/staff.dart';
 
-import 'package:dart_week12/dog.dart';
+//import 'package:dart_week12/dog.dart';
+import 'package:dart_week12/notebook.dart';
 
 void main() {
  //staff();
  //calcat();
- doginfo();
+ //doginfo();
+ callNotebook();
 }
