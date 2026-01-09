3692f53699dbe8e45ddc2717ed2e6d7ea574e3ac
hitori1253 <jont2514@gmail.com>
2026-01-09 12:18:16 +0700

    inher

--- bin/dart_week12.dart
+++ bin/dart_week12.dart
@@ -2,11 +2,14 @@
 //import 'package:dart_week12/staff.dart';
 
 //import 'package:dart_week12/dog.dart';
-import 'package:dart_week12/notebook.dart';
+//import 'package:dart_week12/notebook.dart';
+
+import 'package:dart_week12/person.dart';
 
 void main() {
  //staff();
  //calcat();
  //doginfo();
- callNotebook();
+ //callNotebook();
+ callStudent();
 }
