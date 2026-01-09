6fafbd944e07a45bec79c053c2c61674803c31e6
hitori1253 <jont2514@gmail.com>
2026-01-09 12:38:21 +0700

    อัพเดท

--- bin/dart_week12.dart
+++ bin/dart_week12.dart
@@ -4,12 +4,15 @@
 //import 'package:dart_week12/dog.dart';
 //import 'package:dart_week12/notebook.dart';
 
-import 'package:dart_week12/person.dart';
+//import 'package:dart_week12/person.dart';
+
+import 'package:dart_week12/car.dart';
 
 void main() {
  //staff();
  //calcat();
  //doginfo();
  //callNotebook();
- callStudent();
+ //callStudent();
+ callModel(); 
 }
